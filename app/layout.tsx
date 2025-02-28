2f04e463e1b71f7b89c99bd9b003af10e4dc2ad2
Adem <adem@poool.fr>
2025-02-28 18:05:32 +0100

    chore: init project

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Kollection | Personal vinyl and CD collection",
+  description: "A personal collection of vinyl and CDs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-8 items-center justify-center min-h-screen bg-black text-white">
+      <h1 className="text-8xl font-normal">Kollection</h1>
+      <p className="text-2xl">personal vinyl and cd collection</p>
+    </div>
+  );
+}
