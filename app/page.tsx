6c0b1dcd8c22875e534d22479fa4bdfd7d26a271
Adem <adem@poool.fr>
2025-03-21 11:10:09 +0100

    feat: adding homepage info

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@ export default function Home () {
   return (
     <div
       className={'flex flex-col gap-8 items-center justify-center ' +
-      'min-h-screen bg-white text-black'}
+      'min-h-screen p-16'}
     >
-      <h1 className="text-8xl font-normal">Kollection</h1>
-      <p className="text-2xl">personal vinyl and cd collection</p>
+      <h1 className="text-5xl font-normal">Kollection</h1>
+      <p className="text-center">
+        Choose which part of the kollection you wanna see.
+      </p>
+      <div className="flex justify-center items-center gap-12">
+        <a className="" href="/vinyl">Vinyl</a>
+        <a href="/cd">CD</a>
+      </div>
     </div>
   );
 }
